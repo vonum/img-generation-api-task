5229bfc44f6708e84b9fa87a40a7d0775759f24d
vonum <vonum.mk@gmail.com>
2024-11-27 15:41:22 +0100

    Refactor reader tests

--- image/reader_test.go
+++ image/reader_test.go
@@ -10,79 +10,81 @@ import (
 	"github.com/tutti-ch/backend-coding-task-template/image"
 )
 
-func TestReadBytesAboveThreshold(t *testing.T) {
-  imgBytes, _ := os.ReadFile("../testdata/testimage_big.jpg")
+func TestReadBytes(t *testing.T) {
+  t.Run("Size Above Treshold", func(t *testing.T) {
+    imgBytes, _ := os.ReadFile("../testdata/testimage_big.jpg")
 
-  var buf bytes.Buffer
-  multipartWriter := CreateMultiPart(
-    "image/jpeg",
-    imgBytes,
-    &buf,
-  )
+    var buf bytes.Buffer
+    multipartWriter := CreateMultiPart(
+      "image/jpeg",
+      imgBytes,
+      &buf,
+    )
 
-  r := httptest.NewRequest(http.MethodPost, "/upload", &buf)
-  w := httptest.NewRecorder()
-  r.Header.Set("Content-Type", multipartWriter.FormDataContentType())
+    r := httptest.NewRequest(http.MethodPost, "/upload", &buf)
+    w := httptest.NewRecorder()
+    r.Header.Set("Content-Type", multipartWriter.FormDataContentType())
 
-  _, err := image.ReadBytes(w, r, image.MaxImageSize)
+    _, err := image.ReadBytes(w, r, image.MaxImageSize)
 
-  errMsg := "Image size exceeded - 8192 kB."
-  if err == nil {
-    t.Errorf("Expected to return error: %s", errMsg)
-  }
+    errMsg := "Image size exceeded - 8192 kB."
+    if err == nil {
+      t.Errorf("Expected to return error: %s", errMsg)
+    }
 
-  if err.Error() != errMsg {
-    t.Errorf("Expected error: %s, but got %s", errMsg, err.Error())
-  }
-}
+    if err.Error() != errMsg {
+      t.Errorf("Expected error: %s, but got %s", errMsg, err.Error())
+    }
+  })
 
-func TestReadBytesWrongMimeType(t *testing.T) {
-  textBytes, _ := os.ReadFile("../testdata/text.txt")
+  t.Run("Wrong Mime Type", func(t *testing.T) {
+    textBytes, _ := os.ReadFile("../testdata/text.txt")
 
-  var buf bytes.Buffer
-  multipartWriter := CreateMultiPart(
-    "plain/text",
-    textBytes,
-    &buf,
-  )
+    var buf bytes.Buffer
+    multipartWriter := CreateMultiPart(
+      "plain/text",
+      textBytes,
+      &buf,
+    )
 
-  r := httptest.NewRequest(http.MethodPost, "/upload", &buf)
-  w := httptest.NewRecorder()
-  r.Header.Set("Content-Type", multipartWriter.FormDataContentType())
+    r := httptest.NewRequest(http.MethodPost, "/upload", &buf)
+    w := httptest.NewRecorder()
+    r.Header.Set("Content-Type", multipartWriter.FormDataContentType())
 
-  _, err := image.ReadBytes(w, r, image.MaxImageSize)
+    _, err := image.ReadBytes(w, r, image.MaxImageSize)
 
-  errMsg := "Unsupported mime type - Only .jpeg is allowed."
-  if err == nil {
-    t.Errorf("Expected to return error: %s", errMsg)
-  }
+    errMsg := "Unsupported mime type - Only .jpeg is allowed."
+    if err == nil {
+      t.Errorf("Expected to return error: %s", errMsg)
+    }
 
-  if err.Error() != errMsg {
-    t.Errorf("Expected error: %s, but got %s", errMsg, err.Error())
-  }
-}
+    if err.Error() != errMsg {
+      t.Errorf("Expected error: %s, but got %s", errMsg, err.Error())
+    }
+  })
 
-func TestReadBytesOk(t *testing.T) {
-  imgBytes, _ := os.ReadFile("../testdata/testimage_smalljpg")
+  t.Run("Successful Read", func(t *testing.T) {
+    imgBytes, _ := os.ReadFile("../testdata/testimage_smalljpg")
 
-  var buf bytes.Buffer
-  multipartWriter := CreateMultiPart(
-    "image/jpeg",
-    imgBytes,
-    &buf,
-  )
+    var buf bytes.Buffer
+    multipartWriter := CreateMultiPart(
+      "image/jpeg",
+      imgBytes,
+      &buf,
+    )
 
-  r := httptest.NewRequest(http.MethodPost, "/upload", &buf)
-  w := httptest.NewRecorder()
-  r.Header.Set("Content-Type", multipartWriter.FormDataContentType())
+    r := httptest.NewRequest(http.MethodPost, "/upload", &buf)
+    w := httptest.NewRecorder()
+    r.Header.Set("Content-Type", multipartWriter.FormDataContentType())
 
-  resultBytes, err := image.ReadBytes(w, r, image.MaxImageSize)
+    resultBytes, err := image.ReadBytes(w, r, image.MaxImageSize)
 
-  if err != nil {
-    t.Error("Expected to read bytes successfully")
-  }
+    if err != nil {
+      t.Error("Expected to read bytes successfully")
+    }
 
-  if len(imgBytes) != len(resultBytes) {
-    t.Error("Expected result bytes to be equal to the request bytes")
-  }
+    if len(imgBytes) != len(resultBytes) {
+      t.Error("Expected result bytes to be equal to the request bytes")
+    }
+  })
 }
